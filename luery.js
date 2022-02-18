39fe6a0978040ebe9e2b6c1ce68aca833a6ceea7
LJM12914 <LJM12914@outlook.com>
2022-02-18 23:27:54 +0800

    1.2.2
    部分重构代码，减小体积。

--- luery.js
+++ luery.js
@@ -85,129 +85,140 @@ console.log("luery.js ©LJM12914\r\nhttps://github.com/ljm12914");
             return n - s;
         },
         //获取元素各种坐标信息的封装函数
-        //client<height/width>：内容+padding
+        //client<height/width> 和 ：内容+padding
+        //getBoundingClientRect().<width/height>：内容+padding+border
         //offset<height/width>：内容+padding+border+滚动条（如果有）
         //outer<height/width>：内容+padding+border+滚动条（如果有）+margin
         //F(ixed)T(op)：相对于当前页面上端的坐标，T(op)：相对于网页上端的坐标，B、L、R一样
         //P(ure)H(eight)：元素本身的高度（盒模型最里面那个纯高度，不包括padding margin border scroll）
         //A(ll)H(eight)：元素所有的高度（outer<height/width>）
         tt:(o,t)=>{
+            var h = luery.tt;
             switch(t){
-                case "t": return parseInt(o.css("top").replace("px",""));
-                case "ft": return o.getBoundingClientRect().top;
-                case "b": return luery.tt(o,"t") + luery.tt(o,"h");
-                case "fb": return o.getBoundingClientRect().bottom;
-                case "l": return parseInt(o.css("left").replace("px",""));
-                case "fl": return o.getBoundingClientRect().left;
-                case "r": return luery.tt(o,"l") + luery.tt(o,"w");
-                case "fr": return o.getBoundingClientRect().right;
-                case "h": return o.offsetHeight;
-                case "w": return o.offsetWidth;
+                case "t": return d("top");
+                case "ft": return b("top");
+                case "b": return h(o,"t") + h(o,"h");
+                case "fb": return b("bottom");
+                case "l": return d("left");
+                case "fl": return b("left");
+                case "r": return h(o,"l") + h(o,"w");
+                case "fr": return b("right");
                 case "ah": return o.outerHeight;
                 case "aw": return o.outerWidth;
-                case "ph": return o.clientHeight - parseInt(o.css("padding-top").replace("px","")) - parseInt(o.css("padding-bottom").replace("px",""));
-                case "pw": return o.clientWidth - parseInt(o.css("padding-left").replace("px","")) - parseInt(o.css("padding-right").replace("px",""));
+                case "h": return o.offsetHeight;
+                case "w": return o.offsetWidth;
+                case "dh": return o.clientHeight;
+                case "dw": return o.clientWidth;
+                case "ph": return b("height") - d("padding-top") - d("padding-bottom") - d("border-top-width") - d("border-bottom-width");
+                case "pw": return b("width") - d("padding-left") - d("padding-right") - d("border-left-width") - d("border-right-width");
                 default: luery.E();
             }
+            function d(a){return parseInt(o.css(a).replace("px",""));}
+            function b(a){return eval("o.getBoundingClientRect()." + a);}
         }
     });
 })();
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-NodeList.p = NodeList.prototype;
-NodeList.p.css=function(a,b){//只允许set，因为get多个可能出现冲突
-    if(b === undefined && !luery.isJSONObject(a)) luery.E();
-    for(let i = 0; i < this.length; i++){
-        if(b !== undefined) this[i].style.setProperty(a,b);
-        else for(let k in a) this[i].style.setProperty(k,a[k]);//必然是JSON了
-    }
-    return this;
-}
-//.prototype.hasClass=function(c){} 不可能同时给一大堆元素判断是否有class吧
-NodeList.p.addClass=function(c){
-    for(let i = 0; i < this.length; i++) this[i].addClass(c);
-    return this;
-}
-NodeList.p.removeClass=function(c){
-    for(let i = 0; i < this.length; i++) this[i].removeClass(c);
-    return this;
-}
-NodeList.p.hide=function(){
-    this.css("display","none");
-    return this;
-}
-NodeList.p.show=function(){
-    this.css("display","");
-    return this;
-}
-NodeList.p.setAttribute=function(k,v){
-    for(let i = 0; i < this.length; i++) this[i].setAttribute(k,v);
-    return this;
-}
-NodeList.p.removeAttribute=function(k){
-    for(let i = 0; i < this.length; i++) this[i].removeAttribute(k);
-    return this;
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-if(HTMLElement && SVGElement){
-    SVGElement.p=SVGElement.prototype;
-    HTMLElement.p=HTMLElement.prototype;
-    SVGElement.p.css=HTMLElement.p.css=function(a,b){
-        if(b !== undefined){this.style.setProperty(a,b); return this;}
-        else if(luery.isJSONObject(a)){
-            for(let k in a) this.style.setProperty(k,a[k]);
+(_=>{
+    var $ = luery;
+    if(NodeList){
+        let np = NodeList.prototype;
+        np.css=function(a,b){//只允许set，因为get多个可能出现冲突
+            if(b === undefined && !$.isJSONObject(a)) $.E();
+            for(let i = 0; i < this.length; i++){
+                if(b !== undefined) this[i].style.setProperty(a,b);
+                else for(let k in a) this[i].style.setProperty(k,a[k]);//必然是JSON了
+            }
             return this;
         }
-        else{//本来需要转驼峰才能正常，不知为何不转也可以
-            if(getComputedStyle) return document.defaultView.getComputedStyle(this,false)[a];
-            else return Element.currentStyle[a];
+        //.prototype.hasClass=function(c){} 不可能同时给一大堆元素判断是否有class吧
+        np.addClass=function(c){
+            for(let i = 0; i < this.length; i++) this[i].addClass(c);
+            return this;
+        }
+        np.removeClass=function(c){
+            for(let i = 0; i < this.length; i++) this[i].removeClass(c);
+            return this;
+        }
+        np.hide=function(){
+            this.css("display","none");
+            return this;
+        }
+        np.show=function(){
+            this.css("display","");
+            return this;
+        }
+        np.setAttribute=function(k,v){
+            for(let i = 0; i < this.length; i++) this[i].setAttribute(k,v);
+            return this;
+        }
+        np.removeAttribute=function(k){
+            for(let i = 0; i < this.length; i++) this[i].removeAttribute(k);
+            return this;
         }
     }
-    SVGElement.p.hasClass=HTMLElement.p.hasClass=function(c){return !!this.className.match(new RegExp("(\\s|^)" + c + "(\\s|$)"));}
-    SVGElement.p.addClass=HTMLElement.p.addClass=function(c){
-        if(!c) luery.E();
-        if(this.className == "") this.className += c;
-        else this.className += " " + c;
-        return this;
-    }
-    SVGElement.p.removeClass=HTMLElement.p.removeClass=function(c){
-        if(!!c.match(" ")) luery.E();
-        if(this.hasClass(c)){
-            this.className = this.className.replace(c,"");
-            this.className = this.className.replace(/^\s+|\s+$/g,"");
-            this.className = this.className.replace("  "," ");
+    else lueryFail();
+
+    if(HTMLElement && SVGElement){
+        let sp = SVGElement.prototype,
+        hp = HTMLElement.prototype;
+        sp.css=hp.css=function(a,b){
+            if(b !== undefined){this.style.setProperty(a,b); return this;}
+            else if($.isJSONObject(a)){
+                for(let k in a) this.style.setProperty(k,a[k]);
+                return this;
+            }
+            else{//本来需要转驼峰才能正常，不知为何不转也可以
+                if(getComputedStyle) return document.defaultView.getComputedStyle(this,false)[a];
+                else return Element.currentStyle[a];
+            }
         }
-        return this;
-    }
-    SVGElement.p.parent=HTMLElement.p.parent=function(){return this.parentNode || this.parentElement;}
-    SVGElement.p.hide=HTMLElement.p.hide=function(){this.css("display","none");return this;}
-    SVGElement.p.show=HTMLElement.p.show=function(){this.css("display","unset");return this;}
-    SVGElement.p.isInClass=HTMLElement.p.isInClass=function(c){
-        if(!c) luery.E();
-        let o = this;
-        while(true){
-            if(o.hasClass(c)) return true;
-            if(o.tagName == "HTML") return false;
-            o = o.parent();
+        sp.hasClass=hp.hasClass=function(c){return !!this.className.match(new RegExp("(\\s|^)" + c + "(\\s|$)"));}
+        sp.addClass=hp.addClass=function(c){
+            if(!c) $.E();
+            if(this.className == "") this.className += c;
+            else this.className += " " + c;
+            return this;
+        }
+        sp.removeClass=hp.removeClass=function(c){
+            if(!!c.match(" ")) $.E();
+            if(this.hasClass(c)){
+                this.className = this.className.replace(c,"");
+                this.className = this.className.replace(/^\s+|\s+$/g,"");
+                this.className = this.className.replace("  "," ");
+            }
+            return this;
+        }
+        sp.parent=hp.parent=function(){return this.parentNode || this.parentElement;}
+        sp.hide=hp.hide=function(){this.css("display","none");return this;}
+        sp.show=hp.show=function(){this.css("display","unset");return this;}
+        sp.isInClass=hp.isInClass=function(c){
+            if(!c) $.E();
+            let o = this;
+            while(true){
+                if(o.hasClass(c)) return true;
+                if(o.tagName == "HTML") return false;
+                o = o.parent();
+            }
+        }
+        sp.isInId=hp.isInId=function(c){
+            if(!c) $.E();
+            let o = this;
+            while(true){
+                if(o.id === c) return true;
+                if(o.tagName == "HTML") return false;
+                o = o.parent();
+            }
+        }
+        sp.getParentByClass=hp.getParentByClass=function(c){
+            if(!c) $.E();
+            let o = this;
+            while(true){
+                if(o.hasClass(c)) return o;
+                if(o.tagName == "HTML") return null;
+                o = o.parent();
+            }
         }
     }
-    SVGElement.p.isInId=HTMLElement.p.isInId=function(c){
-        if(!c) luery.E();
-        let o = this;
-        while(true){
-            if(o.id === c) return true;
-            if(o.tagName == "HTML") return false;
-            o = o.parent();
-        }
-    }
-    SVGElement.p.getParentByClass=HTMLElement.p.getParentByClass=function(c){
-        if(!c) luery.E();
-        let o = this;
-        while(true){
-            if(o.hasClass(c)) return o;
-            if(o.tagName == "HTML") return null;
-            o = o.parent();
-        }
-    }
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-else lueryFail();//可自定义错误函数
+    else lueryFail();
+})();
